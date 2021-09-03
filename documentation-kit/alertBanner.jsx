d78480aa69c7f9f74bf8698de3be27534c351495
BrianAA <brianalfaro@thoughttolife.com>
2021-09-03 11:35:26 -0400

    Bumped SC, Fixed Mobile sidebar color, Updated docs(Drawer & Avatar)
    - Bumped SC
    - Finished design docs for (drawer & avatars)
    - Fixed mobile sidebar...(colors were off)

--- documentation-kit/alertBanner.jsx
+++ documentation-kit/alertBanner.jsx
@@ -4,13 +4,35 @@ import { Alert16 } from "@washingtonpost/site-components/core/icon/alert/16";
 import { Check16 } from "@washingtonpost/site-components/core/icon/check/16";
 import { Info16 } from "@washingtonpost/site-components/core/icon/info/16";
 
-export default function alertBanner() {
-     
+export default function alertBanner(props) {
+     const{type}=props;
+     let styles={background:"",fill:"",textColor:""}
+
+     switch (type) {
+         case 'informative':
+             styles={background:"bg-blue-pale",fill:"blue-dark",textColor:"blue-dark"}
+             break;
+        case 'warning':
+            styles={background:"bg-orange-pale",fill:"orange-dark",textColor:"orange-dark"}
+            break;
+        case 'error':
+            styles={background:"bg-red-pale",fill:"red-dark",textColor:"red-dark"}
+            break;
+        case 'success':
+            styles={background:"bg-green-pale",fill:"green-dark",textColor:"green-dark"}
+            break;
+         default:
+            styles={background:"bg-blue-pale",fill:"blue-dark",textColor:"blue-dark"}
+             break;
+     }
     return (
-        <Banner renderCloseButton={false} className="bg-blue-pale brad-4 shadow-1 mt-xl mb-xl w-100">
-            <p className="flex">
-                <Alert16 className="fill-blue-dark mr-xs" />
-                <span className="offblack">Warning that this component currently does not support any transition</span>
+        <Banner renderCloseButton={false} className={`${styles.background} brad-4 shadow-1 mt-xl mb-xl w-100`}>
+            <p className="flex"> 
+                {type=='informative'&&<Info16 className={`fill-${styles.fill} mr-xs`} />}
+                {type=='warning'&&<Alert16 className={`fill-${styles.fill} mr-xs`} />}
+                {type=='success'&&<Check16s className={`fill-${styles.fill} mr-xs`} />}
+
+                <span className={`${styles.textColor}`}>{props.children}</span>
             </p>
         </Banner>
     )
