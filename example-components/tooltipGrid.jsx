429b716dd7d93c3867f4d7d3b78c0c7710d1f681
BrianAA <brianalfaro@thoughttolife.com>
2021-09-29 09:20:51 -0400

    Fix: Corrected casing
    Fixed casing and import path for tooltip to correct failing build

--- example-components/tooltipGrid.jsx
+++ example-components/tooltipGrid.jsx
@@ -1,6 +1,6 @@
 import React,{useState} from 'react'
 import {Button} from '@washingtonpost/site-components/core/button'
-import {Tooltip} from "@washingtonpost/site-components/core/Tooltip/"
+import {Tooltip} from "@washingtonpost/site-components/core/tooltip/"
 function GridSet() {
     const [Popover, setPopover] = useState(false)
     return (
