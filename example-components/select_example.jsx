a2ce99764318e41e151bd5eeb3166fd4ae144cc5
BrianAA <brianalfaro@thoughttolife.com>
2021-09-29 11:15:33 -0400

    Docs:Final Design docs
    Tooltip finished

--- example-components/select_example.jsx
+++ example-components/select_example.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { SSRProvider } from '@react-aria/ssr'
+
 import {
     Select,
     SelectOption
@@ -6,80 +8,90 @@ import {
 
 function SelectExample() {
     return (
-        <Select label="Label text">
-            <SelectOption>Sample Option #1</SelectOption>
-            <SelectOption>Sample Option #2</SelectOption>
-            <SelectOption>Sample Option #3</SelectOption>
-            <SelectOption>Sample Option #4</SelectOption>
-            <SelectOption>Sample Option #5</SelectOption>
-            <SelectOption>Sample Option #6</SelectOption>
-        </Select>
+        <SSRProvider>
+            <Select label="Label text">
+                <SelectOption>Sample Option #1</SelectOption>
+                <SelectOption>Sample Option #2</SelectOption>
+                <SelectOption>Sample Option #3</SelectOption>
+                <SelectOption>Sample Option #4</SelectOption>
+                <SelectOption>Sample Option #5</SelectOption>
+                <SelectOption>Sample Option #6</SelectOption>
+            </Select>
+        </SSRProvider>
     )
 }
 
 function ErrorExample() {
     return (
-        <Select
-        errorMessage="Please select a country"
-        isValid={false}
-        label="Label text">
-            <SelectOption>Sample Option #1</SelectOption>
-            <SelectOption>Sample Option #2</SelectOption>
-            <SelectOption>Sample Option #3</SelectOption>
-            <SelectOption>Sample Option #4</SelectOption>
-            <SelectOption>Sample Option #5</SelectOption>
-            <SelectOption>Sample Option #6</SelectOption>
-        </Select>
+        <SSRProvider>
+            <Select
+                errorMessage="Please select a country"
+                isValid={false}
+                label="Label text">
+                <SelectOption>Sample Option #1</SelectOption>
+                <SelectOption>Sample Option #2</SelectOption>
+                <SelectOption>Sample Option #3</SelectOption>
+                <SelectOption>Sample Option #4</SelectOption>
+                <SelectOption>Sample Option #5</SelectOption>
+                <SelectOption>Sample Option #6</SelectOption>
+            </Select>
+        </SSRProvider>
     )
 }
 
 function DisabledExample() {
     return (
-        <Select isDisabled={true} label="Label text">
-            <SelectOption>Sample Option #1</SelectOption>
-            <SelectOption>Sample Option #2</SelectOption>
-            <SelectOption>Sample Option #3</SelectOption>
-            <SelectOption>Sample Option #4</SelectOption>
-            <SelectOption>Sample Option #5</SelectOption>
-            <SelectOption>Sample Option #6</SelectOption>
-        </Select>
+        <SSRProvider>
+            <Select isDisabled={true} label="Label text">
+                <SelectOption>Sample Option #1</SelectOption>
+                <SelectOption>Sample Option #2</SelectOption>
+                <SelectOption>Sample Option #3</SelectOption>
+                <SelectOption>Sample Option #4</SelectOption>
+                <SelectOption>Sample Option #5</SelectOption>
+                <SelectOption>Sample Option #6</SelectOption>
+            </Select>
+        </SSRProvider>
     )
 }
 
 function MonthExample() {
     return (
-        <Select label="Favorite month">
-            <SelectOption>Jan.</SelectOption>
-            <SelectOption>Feb.</SelectOption>
-            <SelectOption>Mar.</SelectOption>
-            <SelectOption>Apr.</SelectOption>
-            <SelectOption>May.</SelectOption>
-            <SelectOption>Jun.</SelectOption>
-            <SelectOption>Jul.</SelectOption>
-            <SelectOption>Aug.</SelectOption>
-            <SelectOption>Sept.</SelectOption>
-            <SelectOption>Oct.</SelectOption>
-            <SelectOption>Nov.</SelectOption>
-            <SelectOption>Dev.</SelectOption>
-        </Select>
+        <SSRProvider>
+            <Select style={{zIndex:100}} label="Favorite month">
+                <SelectOption>Jan.</SelectOption>
+                <SelectOption>Feb.</SelectOption>
+                <SelectOption>Mar.</SelectOption>
+                <SelectOption>Apr.</SelectOption>
+                <SelectOption>May.</SelectOption>
+                <SelectOption>Jun.</SelectOption>
+                <SelectOption>Jul.</SelectOption>
+                <SelectOption>Aug.</SelectOption>
+                <SelectOption>Sept.</SelectOption>
+                <SelectOption>Oct.</SelectOption>
+                <SelectOption>Nov.</SelectOption>
+                <SelectOption>Dev.</SelectOption>
+            </Select>
+        </SSRProvider>
     )
 }
 
 function PinkThings() {
     return (
-        <Select label="Favorite Pink Thing">
-            <SelectOption>Patrick Star</SelectOption>
-            <SelectOption>Bubble gum</SelectOption>
-            <SelectOption>Flamingo</SelectOption>
-            <SelectOption>Kirby</SelectOption>
-            <SelectOption>Pink Panther</SelectOption>
-            <SelectOption>Slowpoke</SelectOption>
-            <SelectOption>Eraser</SelectOption>
-            <SelectOption>Pencil Bubble gum</SelectOption>
-        </Select>
+        <SSRProvider>
+            <Select className="z-5" label="Favorite Pink Thing">
+                <SelectOption>Patrick Star</SelectOption>
+                <SelectOption>Bubble gum</SelectOption>
+                <SelectOption>Flamingo</SelectOption>
+                <SelectOption>Kirby</SelectOption>
+                <SelectOption>Pink Panther</SelectOption>
+                <SelectOption>Slowpoke</SelectOption>
+                <SelectOption>Eraser</SelectOption>
+                <SelectOption>Pencil Bubble gum</SelectOption>
+            </Select>
+        </SSRProvider>
     )
 }
 
 
-export {SelectExample,PinkThings,MonthExample,DisabledExample,ErrorExample}
+export { SelectExample, PinkThings, MonthExample, DisabledExample, ErrorExample }
 
