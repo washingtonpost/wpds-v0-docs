cd833161c263ee3fae1cff66724f66ae6a62e3af
BrianAA <brianalfaro@thoughttolife.com>
2021-10-21 11:50:41 -0400

    Docs: Updated Design + Dev
    Dev and Design docs have been updated to expose sub sections in the side nav
    
    //ToDo add Button Icon to dev docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -145,13 +145,13 @@ module.exports = {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           editUrl:
-            "https://github.com/WPMedia/WPDS-Docs/tree/main/src/wpds-docs/"
+            "https://github.com/WPMedia/WPDS-Docs/tree/main/"
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            "https://github.com/WPMedia/WPDS-Docs/tree/main/src/wpds-docs/blog/"
+            "https://github.com/WPMedia/WPDS-Docs/tree/main/"
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css")
