ac2ebf56df07aa73c395df091147382133736d13
artmsilva <artmsilva@gmail.com>
2021-12-27 18:28:31 -0500

    boop111

--- pages/boop.js
+++ pages/boop.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { VisuallyHidden } from "@washingtonpost/wpds-ui-kit";
-import { getDocsListBySection } from "~/services";
+// import { getDocsListBySection } from "~/services";
 
 export default function Boop({ posts, docs, foundations, release_notes }) {
   return (
@@ -10,13 +10,13 @@ export default function Boop({ posts, docs, foundations, release_notes }) {
   );
 }
 
-export async function getStaticProps() {
-  const [posts, docs, foundations, release_notes] = [
-    "blog",
-    "components",
-    "foundations",
-    "release-notes"
-  ].map(section => getDocsListBySection(section));
+// export async function getStaticProps() {
+//   const [posts, docs, foundations, release_notes] = [
+//     "blog",
+//     "components",
+//     "foundations",
+//     "release-notes"
+//   ].map(section => getDocsListBySection(section));
 
-  return { props: { posts, docs, foundations, release_notes } };
-}
+//   return { props: { posts, docs, foundations, release_notes } };
+// }
