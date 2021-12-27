bf4980ab982baa30163754eff91ffa222ee49428
artmsilva <artmsilva@gmail.com>
2021-12-27 15:31:13 -0500

    boop revert this

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,4 @@
-import Link from "next/link";
 import { VisuallyHidden, styled } from "@washingtonpost/wpds-ui-kit";
-import Content from "~/components/Layout/Components/Content";
-import { NavigationBar } from "~/components/NavigationBar";
-import { getDocsListBySection } from "~/services";
 
 export default function Index({ posts, docs, foundations, release_notes }) {
   const Boop = styled("span", {
@@ -41,71 +37,6 @@ export default function Index({ posts, docs, foundations, release_notes }) {
     <>
       <VisuallyHidden>This should not flash on first page load.</VisuallyHidden>
       <Boop>This should really not flash at all</Boop>
-      <NavigationBar showLogo />
-      <Content id="content">
-        <h1>Home Page</h1>
-
-        <ul>
-          {posts.map(post => (
-            <li key={post.slug}>
-              <Link href={post.slug} forceHref>
-                <a>{post.data.title}</a>
-              </Link>
-            </li>
-          ))}
-        </ul>
-        <h3>Component Docs</h3>
-        <ul>
-          {docs.map(post => (
-            <li key={post.slug}>
-              <Link href={post.slug} forceHref>
-                <a>{post.data.title}</a>
-              </Link>
-            </li>
-          ))}
-        </ul>
-        <h3>Foundations</h3>
-        <ul>
-          {foundations.map(post => (
-            <li key={post.slug}>
-              <Link href={post.slug} forceHref>
-                <a>{post.data.title}</a>
-              </Link>
-            </li>
-          ))}
-        </ul>
-        <h3>Release Notes</h3>
-        <ul>
-          {release_notes.map(post => (
-            <li key={post.slug}>
-              <Link href={post.slug} forceHref>
-                <a>{post.data.title}</a>
-              </Link>
-            </li>
-          ))}
-        </ul>
-        <Link href="/quick-links" forceHref>
-          <a>
-            <h3>Quick Links</h3>
-          </a>
-        </Link>
-        <Link href="/assets" forceHref>
-          <a>
-            <h3>Assets</h3>
-          </a>
-        </Link>
-      </Content>
     </>
   );
 }
-
-export const getStaticProps = async () => {
-  const [posts, docs, foundations, release_notes] = [
-    "blog",
-    "components",
-    "foundations",
-    "release-notes"
-  ].map(section => getDocsListBySection(section));
-
-  return { props: { posts, docs, foundations, release_notes } };
-};
