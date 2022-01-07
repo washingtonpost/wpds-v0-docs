9a6e21533d28e836aaf96086ceb72886045e46b1
artmsilva <artmsilva@gmail.com>
2022-01-06 23:52:43 -0500

    add site map script

--- README.md
+++ README.md
@@ -7,6 +7,7 @@
 
 ## Launch plan
 
-1. launch v0 on https://build.washingtonpost.com and https://v0.build.washingtonpost.com
-2. launch v1 at https://v1.build.washingtonpost.com
+0. add google analytics?
+1. launch v0 on https://build.washingtonpost.com
+2. launch v1 at https://v1.build.washingtonpost.com. then move v0 back to https://v0.wpds.docs.preview.now.washingtonpost.com or https://build.washingtonpost.com/v0
 3. switch over traffic to v1 from v0. use a small site wide banner for 2 - 3 weeks to help with discovery
---
+++ scripts/sitemap.js
@@ -0,0 +1,69 @@
+const fs = require("fs");
+
+const globby = require("globby");
+const prettier = require("prettier");
+
+(async () => {
+	const pagePaths = await globby(["pages/**/*.js", "!pages/_*.js"]);
+	const pageRoutes = pagePaths
+		.filter((pagePath) => !pagePath.includes("[slug]"))
+		.map((pagePath) =>
+			pagePath
+				.replace("pages", "")
+				.replace(".js", "")
+				.replace("/index", "")
+		);
+
+	const blogPaths = await globby(["mdx_blog"]);
+	const blogRoutes = blogPaths.map((postPath) =>
+		postPath.replace("mdx_blog", "/blog").replace(".mdx", "")
+	);
+
+	const compPaths = await globby(["mdx_components"], {});
+	const componentRoutes = compPaths.map((postPath) =>
+		postPath.replace("mdx_components", "/components").replace(".mdx", "")
+	);
+
+	const foundationPaths = await globby(["mdx_foundations"], {});
+	const foundationRoutes = foundationPaths.map((postPath) =>
+		postPath.replace("mdx_foundations", "/foundations").replace(".mdx", "")
+	);
+
+	const releaseNotesPaths = await globby(["mdx_release-notes"], {});
+	const releaseNotesRoutes = releaseNotesPaths.map((postPath) =>
+		postPath
+			.replace("mdx_release-notes", "/release-notes")
+			.replace(".mdx", "")
+	);
+
+	const allRoutes = [
+		...pageRoutes,
+		...blogRoutes,
+		...componentRoutes,
+		...foundationRoutes,
+		...releaseNotesRoutes,
+	].sort();
+
+	const sitemap = `
+  <?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  ${allRoutes
+		.map((route) => {
+			return `
+      <url>
+      <loc>${`https://build.washingtonpost.com${route}`}</loc>
+      </url>
+      `;
+		})
+		.join("")}
+    </urlset>
+    `;
+
+	const formatted = prettier.format(sitemap, {
+		parser: "html",
+	});
+
+	fs.writeFileSync("public/sitemap.xml", formatted);
+
+	console.log("Sitemap generated");
+})();
