dd293ce3c28ea1660cab653fbe412912f7c28b5c
artmsilva <artmsilva@gmail.com>
2022-01-21 18:16:18 -0500

    fix sitemap

--- scripts/sitemap.js
+++ scripts/sitemap.js
@@ -6,38 +6,19 @@ const prettier = require("prettier");
 (async () => {
   const pagePaths = await globby(["pages/**/*.js", "!pages/_*.js"]);
   const pageRoutes = pagePaths
-    .filter((pagePath) => !pagePath.includes("[slug]"))
+    .filter(
+      (pagePath) => !pagePath.includes("/[") && !pagePath.includes("/test")
+    )
     .map((pagePath) =>
       pagePath.replace("pages", "").replace(".js", "").replace("/index", "")
     );
 
-  const blogPaths = await globby(["mdx_blog"]);
-  const blogRoutes = blogPaths.map((postPath) =>
-    postPath.replace("mdx_blog", "/blog").replace(".mdx", "")
-  );
+  const paths = await globby(["docs"]);
+  const docs = paths.map((postPath) => {
+    return postPath.replace("docs", "").replace(".mdx", "");
+  });
 
-  const compPaths = await globby(["mdx_components"], {});
-  const componentRoutes = compPaths.map((postPath) =>
-    postPath.replace("mdx_components", "/components").replace(".mdx", "")
-  );
-
-  const foundationPaths = await globby(["mdx_foundations"], {});
-  const foundationRoutes = foundationPaths.map((postPath) =>
-    postPath.replace("mdx_foundations", "/foundations").replace(".mdx", "")
-  );
-
-  const releaseNotesPaths = await globby(["mdx_release-notes"], {});
-  const releaseNotesRoutes = releaseNotesPaths.map((postPath) =>
-    postPath.replace("mdx_release-notes", "/release-notes").replace(".mdx", "")
-  );
-
-  const allRoutes = [
-    ...pageRoutes,
-    ...blogRoutes,
-    ...componentRoutes,
-    ...foundationRoutes,
-    ...releaseNotesRoutes,
-  ].sort();
+  const allRoutes = [...pageRoutes, ...docs].sort();
 
   const sitemap = `
   <?xml version="1.0" encoding="UTF-8"?>
