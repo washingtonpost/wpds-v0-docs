2bcebaf5b12c1a1ead4b4aec653ebdb5d2c63f1a
BrianAA <brianalfaro@thoughttolife.com>
2022-01-21 14:25:27 -0500

    Changes to default container
    Added flex, align center, justify content center to ensure that the content is centered.

--- components/Markdown/Components/container.js
+++ components/Markdown/Components/container.js
@@ -3,6 +3,9 @@ import { styled } from "@washingtonpost/wpds-ui-kit";
 export default function Container({ children, caption }) {
 	const Div = styled("div", {
 		width: "100%",
+		display: "flex",
+		alignItems: "center",
+		justifyContent: "center",
 		padding: "$275",
 		backgroundColor: "$gray500",
 		color: "$gray80",
