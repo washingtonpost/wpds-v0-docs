b08e3831fe75e0168b09b883a0365fe2f001a8be
artmsilva <artmsilva@gmail.com>
2022-01-28 10:54:12 -0500

    fix up footer mobile

--- components/Footer.js
+++ components/Footer.js
@@ -15,18 +15,60 @@ export const Footer = () => (
       lineHeight: "$150",
       textAlign: "center",
       color: "$accessible",
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "center",
+      alignItems: "center",
+      gap: "$050",
+
+      "@sm": {
+        display: "grid",
+        gridTemplateAreas: `
+          "footer-top footer-top"
+          "footer-left footer-right"
+        `,
+        gridTemplateColumns: "1fr 1fr",
+        gridTemplateRows: "auto auto",
+        rowGap: "$025",
+        columnGap: "$050",
+      },
     }}
   >
     <Box
       as="a"
       css={{
         color: "$accessible",
+        "@sm": {
+          order: 2,
+          gridArea: "footer-left",
+          textAlign: "right",
+        },
       }}
       href="https://www.washingtonpost.com"
     >
       washingtonpost.com
     </Box>{" "}
-    © 1996-
-    {new Date().getFullYear()} The Washington Post
+    <Box
+      css={{
+        "@sm": {
+          order: 3,
+          gridArea: "footer-right",
+          textAlign: "left",
+        },
+      }}
+    >
+      © 1996-
+      {new Date().getFullYear()}
+    </Box>{" "}
+    <Box
+      css={{
+        "@sm": {
+          order: 1,
+          gridArea: "footer-top",
+        },
+      }}
+    >
+      The Washington Post
+    </Box>
   </Box>
 );
