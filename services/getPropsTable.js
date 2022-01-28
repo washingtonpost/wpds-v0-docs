7a152dfa8e7f5eb116a178583744d65c9ef198d2
artmsilva <artmsilva@gmail.com>
2022-01-27 20:50:26 -0500

    catch error in props table service

--- services/getPropsTable.js
+++ services/getPropsTable.js
@@ -8,28 +8,34 @@ export const getPropsTable = async (component = "icon") => {
   };
 
   // Parse a file for docgen info
-  const [{ props }] = docgen.parse(
-    `node_modules/@washingtonpost/wpds-${component}/src/index.ts`,
-    options
-  );
+  try {
+    const [{ props }] = docgen.parse(
+      `node_modules/@washingtonpost/wpds-${component}/src/index.ts`,
+      options
+    );
 
-  // Get the component's docgen info
-  // object of objects into an array
-  const propsArray = Object.entries(props).map(([key, value]) => ({
-    name: key,
-    type: value.type.name,
-    rawType: value.type.name === "enum" ? value.type.raw.replace(/"/g, "") : "",
-    required: `${value.required}`,
-    description: value.description,
-    defaultValue:
-      value.defaultValue === null
-        ? "----"
-        : JSON.stringify(value.defaultValue, null, 2)
-            .replace(/\\/g, "")
-            .replace(/"/g, "")
-            .replace(/({)|(})|(:)/g, "")
-            .replace(/(value)/g, ""),
-  }));
+    // Get the component's docgen info
+    // object of objects into an array
+    const propsArray = Object.entries(props).map(([key, value]) => ({
+      name: key,
+      type: value.type.name,
+      rawType:
+        value.type.name === "enum" ? value.type.raw.replace(/"/g, "") : "",
+      required: `${value.required}`,
+      description: value.description,
+      defaultValue:
+        value.defaultValue === null
+          ? "----"
+          : JSON.stringify(value.defaultValue, null, 2)
+              .replace(/\\/g, "")
+              .replace(/"/g, "")
+              .replace(/({)|(})|(:)/g, "")
+              .replace(/(value)/g, ""),
+    }));
 
-  return propsArray;
+    return propsArray;
+  } catch (error) {
+    // no component found
+    return {};
+  }
 };
