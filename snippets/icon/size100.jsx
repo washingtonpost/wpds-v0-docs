0e890e9334e0a8654b72ee13c490ba243881dff3
BrianAA <brianalfaro@thoughttolife.com>
2022-03-01 11:06:49 -0500

    zeplin json

---
+++ snippets/icon/size100.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+/**
+ * See documentation for full component api
+```jsx
+//Example
+import { Icon } from "@washingtonpost/wpds-ui-kit"
+import Bookmark from "@washingtonpost/wpds-assets/asset/bookmark";
+
+export default function ExampleSnippet() {
+  return(
+    <Icon size="100">
+      <Bookmark label="bookmark">
+    </Icon>
+)}
+```
+ */
+export default function Icon() {
+  return (
+    <div>See documentation for full example</div>
+  )
+}
