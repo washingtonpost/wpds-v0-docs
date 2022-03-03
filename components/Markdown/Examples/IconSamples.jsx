4e692ff296be3674e04f6db309cb43681afc14ac
Arturo Silva <artmsilva@gmail.com>
2022-03-03 18:19:59 -0500

    bump to latest (#42)
    * bump to latest
    
    * fix api changes
    
    * bump again
    
    * bump again
    
    * bumps
    
    * add stitches to sandbox bumps
    
    * update checkbox examples
    
    * update checkbox examples

--- components/Markdown/Examples/IconSamples.jsx
+++ components/Markdown/Examples/IconSamples.jsx
@@ -1,10 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import * as AllAssets from "@washingtonpost/wpds-assets/asset";
-import { toast } from 'react-toastify';
+import { toast } from "react-toastify";
 import MDXStyling from "~/components/Markdown/Styling";
-import { Grid } from '../Components/Grid';
+import { Grid } from "../Components/Grid";
 import Search from "@washingtonpost/wpds-assets/asset/search";
-import { Icon, theme, Button, AlertBanner, Box, styled } from '@washingtonpost/wpds-ui-kit';
+import {
+  Icon,
+  theme,
+  Button,
+  AlertBanner,
+  Box,
+  styled,
+} from "@washingtonpost/wpds-ui-kit";
 import { paramCase } from "param-case";
 import { logoList } from "./LogoSamples";
 
@@ -21,10 +28,10 @@ const InputHolder = styled("div", {
       },
       false: {
         border: `1px solid ${theme.colors.subtle}`,
-      }
-    }
-  }
-})
+      },
+    },
+  },
+});
 const Input = styled("input", {
   height: "$300",
   border: "none",
@@ -41,8 +48,8 @@ const Input = styled("input", {
       "& + label": {
         fontSize: "$075",
         transform: "translateY(-100%)",
-      }
-    }
+      },
+    },
   },
 
   "&:placeholder-shown": {
@@ -50,7 +57,6 @@ const Input = styled("input", {
       transform: "translateY(-50%)",
       fontSize: "$100",
     },
-
   },
   "&:not(placeholder-shown)": {
     "& + label": {
@@ -58,7 +64,7 @@ const Input = styled("input", {
       fontSize: "$075",
     },
   },
-})
+});
 
 const Label = styled("label", {
   position: "absolute",
@@ -69,25 +75,27 @@ const Label = styled("label", {
   transform: "translateY(-50%)",
   fontWeight: "$light",
   color: theme.colors.accessible,
-  transition: "all .3s ease"
-})
+  transition: "all .3s ease",
+});
 export default function Icons() {
   const SuccessToast = () => {
     return (
       <AlertBanner.Root variant="success">
         <AlertBanner.Content css={{ minWidth: 250, paddingRight: "$050" }}>
-          <b>Copied: {" "}</b>
+          <b>Copied: </b>
           <Box as="span" css={{ fontSize: 16 }}>
-            Import statement for {" "}
-            <Box as="i"
-              css={{ textTransform: "capitalize" }}>{Name}</Box></Box>
+            Import statement for{" "}
+            <Box as="i" css={{ textTransform: "capitalize" }}>
+              {Name}
+            </Box>
+          </Box>
         </AlertBanner.Content>
       </AlertBanner.Root>
-    )
-  }
+    );
+  };
   const [ExampleToCopy, setExampleToCopy] = useState(null);
-  const [Name, setName] = useState("")
-  const [inFocus, setInFocus] = useState(false)
+  const [Name, setName] = useState("");
+  const [inFocus, setInFocus] = useState(false);
   const [Filter, setFilter] = useState("");
   useEffect(() => {
     if (ExampleToCopy) {
@@ -99,17 +107,16 @@ export default function Icons() {
         hideProgressBar: true,
         draggable: false,
         onClose: () => {
-
-          setName(null)
-        }
-      })
+          setName(null);
+        },
+      });
     }
   }, [ExampleToCopy]);
 
   function setVariables(example, Name) {
     console.log(example);
-    setName(Name)
-    setExampleToCopy(example)
+    setName(Name);
+    setExampleToCopy(example);
   }
 
   function handleChange(e) {
@@ -117,46 +124,66 @@ export default function Icons() {
     setFilter(value.toLowerCase());
   }
   const GetIcons = () => {
+    return Object.keys(AllAssets).map((Asset, i) => {
+      const Sample = AllAssets[Asset];
+      const componentName = paramCase(Asset);
 
-    return (
-      Object.keys(AllAssets).map((Asset, i) => {
-        const Sample = AllAssets[Asset];
-        const componentName = paramCase(Asset);
+      const importExample = `import ${Asset.replace(
+        "Svg",
+        ""
+      )} from "@washingtonpost/wpds-assets/asset/${componentName.replace(
+        "svg",
+        ""
+      )}";`;
 
-        const importExample = `import ${Asset.replace(
-          "Svg",
-          ""
-        )} from "@washingtonpost/wpds-assets/asset/${componentName.replace(
-          "svg",
-          ""
-        )}";`;
+      if (logoList.includes(componentName)) return;
+      if (Filter != "" && !componentName.includes(Filter)) return;
+      return (
+        <MDXStyling.Cell key={i}>
+          <Box
+            as="button"
+            onClick={() => setVariables(importExample, componentName)}
+            css={{
+              cursor: "pointer",
+              border: "none",
+              "&:hover": { opacity: 0.5 },
+              backgroundColor: theme.colors.gray500,
+              padding: theme.space[100],
+            }}
+          >
+            <Icon size="$150">
+              <Sample fill={theme.colors.primary} />
+            </Icon>
+          </Box>
+        </MDXStyling.Cell>
+      );
+    });
+  };
 
-        if (logoList.includes(componentName)) return;
-        if (Filter != "" && !componentName.includes(Filter)) return
-        return (
-          <MDXStyling.Cell key={i}>
-            <Box
-              as="button"
-              onClick={() => setVariables(importExample, componentName)}
-              css={{ cursor: "pointer", border: "none", "&:hover": { opacity: .5 }, backgroundColor: theme.colors.gray500, padding: theme.space[100] }}>
-              <Icon size="$150">
-                <Sample fill={theme.colors.primary} />
-              </Icon>
-            </Box>
-          </MDXStyling.Cell >
-        );
-      }))
-  }
-
-  return (<>
-    <InputHolder showFocus={inFocus ? "true" : "false"}>
-      <Input placeholder=" " onFocus={() => setInFocus(true)} onBlur={() => setInFocus(false)} onChange={handleChange} />
-      <Label>Search</Label>
-      <Button as="div" css={{ border: "none", borderRadius: "0" }} variant={"primary"} style="outline">
-        <Icon>
-          <Search />
-        </Icon>
-      </Button>
-    </InputHolder>
-    <Grid><GetIcons /></Grid></>)
+  return (
+    <>
+      <InputHolder showFocus={inFocus ? "true" : "false"}>
+        <Input
+          placeholder=" "
+          onFocus={() => setInFocus(true)}
+          onBlur={() => setInFocus(false)}
+          onChange={handleChange}
+        />
+        <Label>Search</Label>
+        <Button
+          as="div"
+          css={{ border: "none", borderRadius: "0" }}
+          variant={"primary"}
+          isOutline
+        >
+          <Icon>
+            <Search />
+          </Icon>
+        </Button>
+      </InputHolder>
+      <Grid>
+        <GetIcons />
+      </Grid>
+    </>
+  );
 }
