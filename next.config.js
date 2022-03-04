530f18e659e9807eef8136e84fd3fca0429c281c
artmsilva <artmsilva@gmail.com>
2022-03-04 11:24:57 -0500

    set up v0 rewrites

--- next.config.js
+++ next.config.js
@@ -1,18 +1,28 @@
 module.exports = {
   async rewrites() {
-    return [
-      {
-        source: "/components",
-        destination: "/components/design",
-      },
-      {
-        source: "/foundations",
-        destination: "/foundations/principles",
-      },
-      {
-        source: "/",
-        destination: "/foundations",
-      },
-    ];
+    return {
+      beforeFiles: [],
+      afterFiles: [
+        {
+          source: "/components",
+          destination: "/components/design",
+        },
+        {
+          source: "/foundations",
+          destination: "/foundations/principles",
+        },
+        {
+          source: "/",
+          destination: "/foundations",
+        },
+      ],
+      fallback: [
+        {
+          source: "/v0/:slug*",
+          destination:
+            "https://v0.wpds.docs.preview.now.washingtonpost.com/:slug*",
+        },
+      ],
+    };
   },
 };
